69507c8a18d23657bb58dd16aa67a44e67814539
Techie <brickkalu@gmail.com>
2024-02-21 20:42:24 +0100

    Error fixed on Images

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -25,8 +25,8 @@ function LandingPage () {
                     </div>
                     <h3>Download at</h3>
                     <div className="ggle">
-                        <img src="Images/googleplay.png" alt="" />
-                        <img src="Images/appstore.png" alt="" />
+                        <img src="./Images/googleplay.png" alt="" />
+                        <img src="./Images/appstore.png" alt="" />
                     </div>
                 </div>
             </div>
@@ -37,7 +37,7 @@ function LandingPage () {
                             <h2>20,000</h2>
                             <p>People joined Xtremepay</p>
                         </div>
-                        <img src="Images/Line 1.png" alt="" />
+                        <img src="./Images/Line 1.png" alt="" />
                     </div>
                 </div>
                 <div className="join1">
@@ -46,7 +46,7 @@ function LandingPage () {
                             <h2>10,000</h2>
                             <p>Reviews from googleplay</p>
                         </div>
-                        <img src="Images/Line 1.png" alt="" />
+                        <img src="./Images/Line 1.png" alt="" />
                     </div>
                 </div>
                 <div className="join1">
@@ -57,10 +57,10 @@ function LandingPage () {
             <div className="details">
                 <div className="details-text-1">
                     <h2>Why you must use Xtrempay</h2>
-                    <img className="ii" src="Images/Vector 1.png" alt="" />
+                    <img className="ii" src="./Images/Vector 1.png" alt="" />
                     <div className="ggles">
-                        <img src="Images/googleplay.png" alt="" />
-                        <img src="Images/appstore.png" alt="" />
+                        <img src="./Images/googleplay.png" alt="" />
+                        <img src="./Images/appstore.png" alt="" />
                     </div>
                 </div>
                 <div className="details-text">
@@ -97,41 +97,41 @@ function LandingPage () {
                 </div>
                 <div className="ideas-subtext">
                     <div className="ideas-sub">
-                        <img src="Images/Frame 630500.png" alt="" />
+                        <img src="./Images/Frame 630500.png" alt="" />
                         <div className="transfer">
-                            <img src="Images/Line 1.png" alt="" id='l' />
+                            <img src="./Images/Line 1.png" alt="" id='l' />
                             <h2>Transfer</h2>
                         </div>
                         <p>Need to send money to a friend, family member, or colleague? XRemPay's money transfer feature is your go-to solution It allows more transfer.</p>
                     </div>
                     <div className="ideas-sub" id='sub-t'>
-                        <img src="Images/vaadin.png" alt="" />
+                        <img src="/Images/vaadin.png" alt="" />
                         <div className="transfer">
-                            <img src="Images/Line 6.png" alt="" />
+                            <img src="./Images/Line 6.png" alt="" />
                             <h2>Withdrawals</h2>
                         </div>
                         <p>Xtrempay allows you to withdraw money conveniently, ensuring that you have access to your funds whenever and wherever you need them.</p>
                     </div>
                     <div className="ideas-sub" id='sub-t'>
-                        <img src="Images/fluent.png" alt="" id='l' />
+                        <img src="./Images/fluent.png" alt="" id='l' />
                         <div className="transfer">
-                            <img src="Images/Line 6.png" alt="" />
+                            <img src="./Images/Line 6.png" alt="" />
                             <h2>Payments</h2>
                         </div>
                         <p>Whether you're settling bills, making purchases, or paying for services, Xtrempay simplifies the payment processc It helps in easy pay off.</p>
                     </div>
                     <div className="ideas-sub" id='sub-t'>
-                        <img src="Images/savings.png" alt="" />
+                        <img src="./Images/savings.png" alt="" />
                         <div className="transfer">
-                            <img src="Images/Line 6.png" alt="" id='l'/>
+                            <img src="./Images/Line 6.png" alt="" id='l'/>
                             <h2>Savings</h2>
                         </div>
                         <p>Remember, every penny saved is a step closer to your dreams. Start small, be consistent, and witness the compounding effect of financial discipline</p>
                     </div>
                     <div className="ideas-sub" id='sub-t'>
-                        <img src="Images/Vector.png" alt="" />
+                        <img src="./Images/Vector.png" alt="" />
                         <div className="transfer">
-                            <img src="Images/Line 6.png" alt="" />
+                            <img src="./Images/Line 6.png" alt="" />
                             <h2>Cash prizes</h2>
                         </div>
                         <p>Remember, every penny saved is a step closer to your dreams. Start small, be consistent, and witness the compounding effect of financial discipline</p>
@@ -140,51 +140,51 @@ function LandingPage () {
             </div>
             <div className="services">
                 <h2>Services we are offering</h2>
-                <img src="Images/Vector 3.png" alt="" />
+                <img src="./Images/Vector 3.png" alt="" />
                 <div className="services-text">
-                    <img src="Images/Frame 630537.png" alt="" />
+                    <img src="./Images/Frame 630537.png" alt="" />
                     <div className="service-subtext">
                         <h2>Moola</h2>
                         <h3>Save with Excitement Save  and Win Up to 10M weekly</h3>
                         <p>Moola is a financial app that offers prize linked account as an alternative to traditional interest based savings. Moola offers people an exceptional and exciting chance of wining a cash prize in return for saving money into Moola account.</p>
-                        <button type="button">SEE MORE <img src="Images/ic.png" alt="" /></button>
+                        <button type="button">SEE MORE <img src="./Images/ic.png" alt="" /></button>
                     </div>
                 </div>
-                <img className="rec" src="Images/rec.png" alt="" />
+                <img className="rec" src="./Images/rec.png" alt="" />
                 <div className="services-text">
                     <div className="service-subtext">
                         <h2>Passabi</h2>
                         <h3>Made for Small Business Owners</h3>
                         <p>Passabi offers seamless group savings and provide access to no interest loans for individual members that have come together to solve their money challenges. Digitalize rotating savings platform that is seamlessly integrates with commercial bank and fin-tech companies to offers access to secure and easy to use financial services</p>
-                        <button type="button">SEE MORE <img src="Images/ic.png" alt="" /></button>
+                        <button type="button">SEE MORE <img src="./Images/ic.png" alt="" /></button>
                     </div>
-                    <img src="Images/Frame 630538.png" alt="" />
+                    <img src="./Images/Frame 630538.png" alt="" />
                 </div>
             </div>
             <div className="customers">
                 <h2>Here is what our customers think</h2>
                 <div className="customer-text">
-                    <img src="Images/woman.png" alt="" />
+                    <img src="./Images/woman.png" alt="" />
                     <div className="customer-subtext">
                         <p>"From the moment I downloaded XtremPay, I was impressed by its user-friendly interface. Setting up my account was a breeze, and I was quickly navigating through its features with ease. The simplicity of the app, coupled with its powerful functionalities, has made managing my money more convenient than ever."</p>
                         <div className="switch">
-                            <img src="Images/Frame 630495.png" alt="" />
-                            <img src="Images/Frame 630494.png" alt="" />
+                            <img src="./Images/Frame 630495.png" alt="" />
+                            <img src="./Images/Frame 630494.png" alt="" />
                         </div>
                     </div>
                 </div>
-                <img className="eeh" src="Images/Rectangle 1.png" alt="" />
+                <img className="eeh" src="./Images/Rectangle 1.png" alt="" />
             </div>
             <div className="end-end">
-                <img src="Images/Group 34.png" alt="" />
+                <img src="./Images/Group 34.png" alt="" />
                 <div className="end-end-text">
                     <h2>END-TO-END SECURITY</h2>
                     <p>At Xtrempay, we understand that your financial well-being is not just a matter of numbers; it's a matter of trust. That's why we've meticulously crafted an end-to-end security framework, ensuring that your financial data is safeguarded at every stage of your journey.</p>
-                    <li><img src="Images/Frame 630482.png" alt="" />Secure Authentication: Your Digital Identity Shield</li>
-                    <li><img src="Images/Frame 630482 (1).png" alt="" />Encrypted Communication: Safeguarding Your Transactions</li>
-                    <li><img src="Images/Frame 630484.png" alt="" />Device Security: Extending the Shield to Your Device</li>
-                    <li><img src="Images/Frame 630485.png" alt="" />Transaction Integrity: Verifying Every Financial Move</li>
-                    <li><img src="Images/Frame 630486.png" alt="" />Data Residency and Compliance: Meeting Global Standards</li>
+                    <li><img src="./Images/Frame 630482.png" alt="" />Secure Authentication: Your Digital Identity Shield</li>
+                    <li><img src="./Images/Frame 630482 (1).png" alt="" />Encrypted Communication: Safeguarding Your Transactions</li>
+                    <li><img src="./Images/Frame 630484.png" alt="" />Device Security: Extending the Shield to Your Device</li>
+                    <li><img src="./Images/Frame 630485.png" alt="" />Transaction Integrity: Verifying Every Financial Move</li>
+                    <li><img src="./Images/Frame 630486.png" alt="" />Data Residency and Compliance: Meeting Global Standards</li>
                 </div>
             </div>
             <div className="agent">
@@ -193,10 +193,10 @@ function LandingPage () {
                     <p>The easiest and reliable way of providing affordable financial service in Africa</p>
                     <h3>Join us and grow your business fast</h3>
                     <button>
-                        <img src="Images/whatsapp.png" alt="" />Contact to join Xtremepay
+                        <img src="./Images/whatsapp.png" alt="" />Contact to join Xtremepay
                     </button>
                 </div>
-                <img src="Images/pos.png" alt="" />
+                <img src="./Images/pos.png" alt="" />
             </div>
             <ContactTab/>
             <Footer/>
