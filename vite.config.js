bf4712b276718f2875f3e049dcd2c01251d12595
Kalu-Emeka <brickkalu@gmail.com>
2024-08-27 19:29:17 +0100

    Errors

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['wagmi'] // Add wagmi to the external array
+      external: ['wagmi/chains'] // Add wagmi to the external array
     },
   }
 })
